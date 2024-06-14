a16a44e9a16c02d7ca784b9dbd21a998b3655ffb
Benjamin Etheridge <betheridge@turing.ac.uk>
2024-06-14 20:44:36 +0100

    pre-commit setup

--- src/mlfp/__init__.py
+++ src/mlfp/__init__.py
@@ -1,9 +1,11 @@
 """
-Machine Learning from First Principles: Implementing Machine Learning algorithms from first principles, without relying on Python libraries. 
+Machine Learning from First Principles:
+    Implementing Machine Learning algorithms from first principles,
+    without relying on Python libraries.
 """
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __all__ = ("__version__",)
-__version__ = version(__name__) 
+__version__ = version(__name__)
